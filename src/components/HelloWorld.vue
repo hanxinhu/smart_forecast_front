7e7fd21933eed1842ff49dfe0f70a7204cf9a8bf
hanxinhu <hanxinhu521@163.com>
2020-08-11 00:02:21 +0800

    chore: update pdf to img

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,7 +13,7 @@
           <a-input v-if="param.type==='string'" v-model="form.params[i]"/>
           <a-input-number v-if="param.type==='number'" v-model="form.params[i]"/>
           <a-auto-complete v-if="param.type==='list'" v-model="form.params[i]" :data-source="param.values">
-            </a-auto-complete>
+          </a-auto-complete>
         </a-form-model-item>
         <a-button type="primary" @click="submit">
           确认
@@ -25,122 +25,134 @@
       </a-button>
     </div>
     <div>
-      <pdf
-        ref="pdf"
-        src="http://file.dakawengu.com/file/2018-05-29/20180527-tianfeng.pdf">
-      </pdf>
+      <img :src="imgUrl">
     </div>
   </div>
 </template>
 
 <script>
-import pdf from 'vue-pdf'
-export default {
-  name: 'HelloWorld',
-  components:{
-    pdf
-  },
-  data() {
-    return {
-      labelCol: {span: 4},
-      wrapperCol: {span: 12},
-      msg: 'Welcome to Your Vue.js App',
-      configs: [
-        {
-          name: 'name',
-          desc: 'desc',
-          params: [
-            {
-              name: 'test',
-              desc: 'testString',
-              type: 'string',
-              values: '',
-              value: 'test',
-            },
-            {
-              name: 'test',
-              desc: 'testNumber',
-              type: 'number',
-              values: '',
-              value: 'test',
-            },
-            {
-              name: 'test',
-              desc: 'testList',
-              type: 'list',
-              values: ['1','2','3'],
-              value: 'test',
-            }
-          ]
-        },
-        {
-          name: 'name2',
-          desc: 'desc2',
-        }
-      ],
-      params: [],
-      form: {
-        name: '',
-        desc: '',
+
+  export default {
+    name: 'HelloWorld',
+
+    data() {
+      return {
+        labelCol: {span: 4},
+        wrapperCol: {span: 12},
+        msg: 'Welcome to Your Vue.js App',
+        imgUrl: null,
+        configs: [
+          {
+            name: 'name',
+            desc: 'desc',
+            params: [
+              {
+                name: 'test',
+                desc: 'testString',
+                type: 'string',
+                values: '',
+                value: 'test',
+              },
+              {
+                name: 'test',
+                desc: 'testNumber',
+                type: 'number',
+                values: '',
+                value: 'test',
+              },
+              {
+                name: 'test',
+                desc: 'testList',
+                type: 'list',
+                values: ['1', '2', '3'],
+                value: 'test',
+              }
+            ]
+          },
+          {
+            name: 'name2',
+            desc: 'desc2',
+          }
+        ],
         params: [],
-      },
-      tempInput:'',
+        form: {
+          name: '',
+          desc: '',
+          params: [],
+        },
+        tempInput: '',
+      }
+    },
+    created() {
+      this.init();
     }
-  },
-  methods: {
-    a: function () {
-      let _this = this
-      this.$axios.get('/hello').then(e => {
-        _this.msg = e.data
-      })
-    },
-    handleSelect: function (e) {
-      console.log(e)
-      console.log(this.form)
-      this.form.name = this.configs[e].name;
-      this.form.desc = this.configs[e].desc;
-      this.params = this.configs[e].params;
-    },
-    submit: function (e) {
-      e.preventDefault();
-      console.log(this.form)
-    },
-    next: function () {
-      let i = 0;
+    ,
+    methods: {
+      init: function () {
+        let _this = this
+        this.$axios.get('/init').then(e => {
+          _this.configs = e.data
 
-      for (; i < this.configs.length; i++) {
-        if (this.configs[i].name === this.form.name) {
-          break;
+        })
+      },
+      // getFile: function () {
+      //   let _this = this
+      //   this.$axios.get('/file').then(e => {
+      //     _this.pdfFile = e.data
+      //   })
+      // },
+      handleSelect: function (e) {
+        console.log(e)
+        console.log(this.form)
+        this.form.name = this.configs[e].name;
+        this.form.desc = this.configs[e].desc;
+        this.params = this.configs[e].params;
+      },
+      submit: function (e) {
+        e.preventDefault();
+        console.log(this.form)
+        if (this.form.name === this.configs
+          [1].name) {
+          this.imgUrl = 'http://localhost:5000/file'
         }
-      }
+        this.$message.success("成功提交")
+      },
+      next: function () {
+        let i = 0;
 
-      if (i + 1 < this.configs.length) {
-        this.form.name = this.configs[i + 1].name;
-        this.form.desc = this.configs[i + 1].desc;
-        this.params = this.configs[i + 1].params;
-      }
-    },
+        for (; i < this.configs.length; i++) {
+          if (this.configs[i].name === this.form.name) {
+            break;
+          }
+        }
+
+        if (i + 1 < this.configs.length) {
+          this.form.name = this.configs[i + 1].name;
+          this.form.desc = this.configs[i + 1].desc;
+          this.params = this.configs[i + 1].params;
+        }
+      },
+    }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
 
-a {
-  color: #42b983;
-}
+  a {
+    color: #42b983;
+  }
 </style>
